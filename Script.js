59c0cd0dea144f2451307b84c415943f4c3f9772
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-16 12:28:26 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,51 @@
+const makingPizza = function () {
+    console.log("...........................");
+    console.log("Voor verwarm oven op 240 graden");
+    console.log("Maak bodem");
+    console.log("Verdeel tomatensaus over bodem");
+    console.log("Kies beleg en verdeel over de pizza");
+    console.log("Doe pizza in de voorverwarmde oven voor 10 minuten");
+    console.log("Eet pizza");
+    console.log("...........................");
+    console.log("...........................");
+    console.log("Voor verwarm oven op 240 graden");
+    console.log("Maak bodem");
+    console.log("Verdeel tomatensaus over bodem");
+    console.log("Kies beleg en verdeel over de pizza");
+    console.log("Doe pizza in de voorverwarmde oven voor 10 minuten");
+    console.log("Eet pizza");
+    console.log("...........................");
+}
+
+makingPizza();
+
+const makingSushi = function () {
+    console.log("...........................");
+    console.log("Kook water voor rijst");
+    console.log("Weeg 100 gram rijst");
+    console.log("Kook rijst voor 8 minuten");
+    console.log("Giet rijst af");
+    console.log("Bak 3 gambas tot ze licht verkleurd zijn");
+    console.log("Rol de gebakken gambas in de rijst");
+    console.log("Eet Sushi");
+    console.log("...........................");
+    console.log("...........................");
+    console.log("Kook water voor rijst");
+    console.log("Weeg 100 gram rijst");
+    console.log("Kook rijst voor 8 minuten");
+    console.log("Giet rijst af");
+    console.log("Bak 3 gambas tot ze licht verkleurd zijn");
+    console.log("Rol de gebakken gambas in de rijst");
+    console.log("Eet Sushi");
+    console.log("...........................");
+    console.log("...........................");
+    console.log("Kook water voor rijst");
+    console.log("Weeg 100 gram rijst");
+    console.log("Kook rijst voor 8 minuten");
+    console.log("Giet rijst af");
+    console.log("Bak 3 gambas tot ze licht verkleurd zijn");
+    console.log("Rol de gebakken gambas in de rijst");
+    console.log("Eet Sushi");
+    console.log("...........................");
+}
+makingSushi();
